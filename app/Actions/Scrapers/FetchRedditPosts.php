35003ec9d4e95d3b0b8eb489e59139e15907b64e
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2023-03-31 10:26:41 +0200

    Simplified Reddit output

--- app/Actions/Scrapers/FetchRedditPosts.php
+++ app/Actions/Scrapers/FetchRedditPosts.php
@@ -52,7 +52,7 @@ class FetchRedditPosts
         $this->clear();
         $this->append('# What happened on Reddit?');
         $this->append("There are quite a few relevant subreddits, but we'll focus on the 2 most popular and active ones to start with. To narrow the scope further, we'll restrict the posts to links only. Based on my initial investigation this should show the most value.");
-        $this->append("\n");
+        $this->newLine();
 
         foreach (self::SUBREDDITS as $subreddit) {
             $this->fetch($subreddit);
@@ -63,6 +63,12 @@ class FetchRedditPosts
     {
         $this->command->line($line);
         Storage::append($this->filepath, $line);
+    }
+
+    private function newLine(): void
+    {
+        $line = "\n";
+        $this->append($line);
     }
 
     private function clear(): void
@@ -82,6 +88,7 @@ class FetchRedditPosts
             $subredditUrl,
             $subreddit
         ));
+        $this->newLine();
 
         $response = Http::acceptJson()->get($subredditUrl, [
             'count' => self::MAX_COUNT,
@@ -93,38 +100,21 @@ class FetchRedditPosts
             ->filter(fn ($post) => ! empty(data_get($post, 'data.url_overridden_by_dest')))
             ->values();
 
-        $this->append("\n");
-
-        // Display as list
         foreach ($posts as $idx => $post) {
-            $created = Carbon::createFromTimestamp(data_get($post, 'data.created'));
-
             $postLink = sprintf(
                 'https://www.reddit.com/r/PHP/comments/%s/',
                 data_get($post, 'data.id')
             );
+
             $this->append(sprintf(
                 '**#%d) <a href="%s">%s</a>**',
                 ($idx + 1),
                 $postLink,
                 data_get($post, 'data.title')
             ));
-            $this->append(sprintf('- Link: %s', data_get($post, 'data.url_overridden_by_dest')));
-            $this->append(sprintf(
-                '- Votes: +%d/-%d (%d%%)',
-                data_get($post, 'data.ups'),
-                data_get($post, 'data.downs'),
-                floatval(data_get($post, 'data.upvote_ratio')) * 100,
-            ));
-
-            $author = data_get($post, 'data.author');
-            $this->append(sprintf(
-                '- Posted on %s by <a href="https://www.reddit.com/user/%s">@%s</a>',
-                $created->format('Y-m-d'),
-                $author,
-                $author
-            ));
-            $this->append("\n");
+            $this->newLine();
+            $this->append(data_get($post, 'data.url_overridden_by_dest'));
+            $this->newLine();
 
             if ($idx >= 9) {
                 break;

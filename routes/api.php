935783aa61c00360dc34270274d1fd1c8fb8be6b
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2023-04-04 19:17:38 +0200

    Created auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,60 @@
 <?php
 
+use App\Exceptions\ApiException;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/token', function (Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+        $user = User::firstWhere('email', $request->email);
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw new ApiException('Invalid credentials');
+        }
+
+        // Revoke existing tokens
+        $user->revokeAllTokens();
+
+        return [
+            'success' => true,
+            'token' => $user->createToken('api', ['api-full'])->plainTextToken,
+        ];
+    });
+
+    Route::post('/register', function (Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required',
+        ]);
+
+        if (User::firstWhere('email', $request->email)) {
+            throw new ApiException('The email already exists');
+        }
+
+        $user = User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'name' => $request->name,
+        ]);
+
+        // TODO: Send email verification here
+        // - Bypassed for now
+        $user->markEmailAsVerified();
+
+        return [
+            'success' => true,
+            'message' => 'Sucessfully registered',
+        ];
+    });
+});
+
+Route::middleware(['auth:sanctum', 'abilities:api-full'])->group(function () {
+    Route::get('/me', fn (Request $request) => $request->user());
 });

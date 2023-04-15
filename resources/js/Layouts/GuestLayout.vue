4f8effe6cde7dc7a77c728f6cab2c5184a851e31
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2023-04-15 13:40:40 +0200

    Starting scaffolding for web interface

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,29 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <!-- Quantity should be the same in Sass-->
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="bg-white bg-opacity-50 p-8 rounded-lg shadow-md w-full max-w-md">
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,32 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+
+
+const submitUrl = () => {
+    console.log('Submit URL');
+}
+
+</script>
+
+<template>
+    <GuestLayout>
+        <h1 class="text-2xl font-bold mb-6">Review Blog Post</h1>
+        <form @submit.prevent="submitUrl()">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="url">
+                    URL
+                </label>
+                <input v-model="url"
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="url" type="text" placeholder="Enter a URL" />
+            </div>
+            <div class="flex items-center justify-between">
+                <button
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit">
+                    Review
+                </button>
+            </div>
+        </form>
+    </GuestLayout>
+</template>
